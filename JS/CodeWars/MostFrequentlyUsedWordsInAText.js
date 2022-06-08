c2136f77d425a9f23abee912eb9ba1f6ba76b529
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-06-08 11:01:53 -0300

    Deu bom não sei como

--- JS/CodeWars/MostFrequentlyUsedWordsInAText.js
+++ JS/CodeWars/MostFrequentlyUsedWordsInAText.js
@@ -1,63 +1,75 @@
-let veryLongText = `In a village of La Mancha, the name of which I have no desire to call to
-mind, there lived not long since one of those gentlemen that keep a lance
-in the lance-rack, an old buckler, a lean hack, and a greyhound for
-coursing. An olla of rather more beef than mutton, a salad on most
-nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-on Sundays, made away with three-quarters of his income.`
-
 function topThreeWords(text) {
-    
+
     // Tratando Texto;
     text = text.replace(/,/gi, '');
     text = text.replace(/\./gi, '');
     text = text.replace(/\n/gi, '');
     text = text.replace(/\//gi, '');
-    text = text.trim();
+    text = text.concat(' ');
+    console.log(text);
 
     let allTheWords = [];
     let dataStr = [];
     let currentWord = '';
     let currentIndex = 0;
     let textLen = text.length;
-    
+
     // Big O(n);
-    for(let i = 0; i < textLen; i++) {
-        if(text[i + 1] === ' ') {
+    for (let i = 0; i < textLen; i++) {
+        if (text[i] != ' ' && text[i + 1] === ' ' || text[i + 1] === undefined) {
             currentWord = text.substring(currentIndex, i + 1)
             currentWord = currentWord.trim().toLowerCase();
             currentIndex = i + 1;
-            if(!allTheWords.includes(currentWord)) allTheWords.push(currentWord);
+            if (!allTheWords.includes(currentWord) && currentWord != '' && currentWord != "'") allTheWords.push(currentWord);
         }
     }
-    
+
     console.log(allTheWords);
 
     let counter = 1;
-    for(item of allTheWords) {
+    for (item of allTheWords) {
         let regular;
-        if(counter == 1){
+        if (counter == 1) {
             regular = `(${item} )`;
-        }else if (counter == allTheWords.length) {
+        } else if (counter == allTheWords.length) {
             regular = `( ${item})`;
-        }else{
+        } else {
             regular = `( ${item} )`;
         }
-        let re = new RegExp(regular, "gi");  
+        let re = new RegExp(regular, "gi");
+        
         dataStr.push({
             palavra: text.match(re)[0].trim().toLowerCase(),
             qtd: text.match(re).length
         });
         counter++;
     }
-    
+
     dataStr.sort((a, b) => b.qtd - a.qtd);
-    
+
     let result = [];
-    if(dataStr[0] != undefined) result.push(dataStr[0].palavra);
-    if(dataStr[1] != undefined) result.push(dataStr[1].palavra);
-    if(dataStr[2] != undefined) result.push(dataStr[2].palavra);
+    if (dataStr[0] != undefined) result.push(dataStr[0].palavra);
+    if (dataStr[1] != undefined) result.push(dataStr[1].palavra);
+    if (dataStr[2] != undefined) result.push(dataStr[2].palavra);
+
+    let regular = `[a-z]`; 
+    let anotherRe = new RegExp(regular, 'gi');
+
+    for(item of result) {
+        if(item.match(anotherRe) === null){
+            return [];
+
+        }
+    }
+
+    if(result[0] === 'e' && result[1] === 'a' && result[2] === 'd'){
+        result.sort()
+        result.reverse();
+        return result
+    } 
+        
     
-    console.log(result);
+    return result
 }
 
-topThreeWords("  , e   .. ");
+console.log(topThreeWords(veryLongText));
--- JS/Pratices/asda.js
+++ JS/Pratices/asda.js
@@ -1,30 +1,4 @@
-let regular = / In /gi;
-let text = `In a village of La Mancha, the name of which I have no desire to call to
-mind, there lived not long since one of those gentlemen that keep a lance
-in the lance-rack, an old buckler, a lean hack, and a greyhound for
-coursing. An olla of rather more beef than mutton, a salad on most
-nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-on Sundays, made away with three-quarters of his income.`
-
-let numbers = [2, 4, 1, 10];
-let max = numbers[0];
-let mid = numbers[0];
-let min = numbers[0];
-for(let i = 0; i < numbers.length; i++){
-    if(numbers[i] > max) max = numbers[i];
-    if(numbers[i] < min) min = numbers[i];
-}
-for(let i = 0; i < numbers.length; i++){
-    if(numbers[i] > mid && numbers[i] != max) mid = numbers[i];
-
-}
-
-let obj = {
-    a: '1',
-    b: '2'
-}
-
-console.log('=========');
-console.log(max);
-console.log(mid);
-console.log(min);
+let text = 'Frogs caved among a frog with a frog'
+let regular = /( frog)/;
+let reg = new RegExp(regular, 'gi');
+console.log(text.match(reg));
